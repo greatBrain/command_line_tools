d65fac8c7a2ddf6fb347f226ccc2200e990c704d
greatbrain <erickfania@gmail.com>
2022-12-18 09:06:32 -0400

    temporary file creator working

--- temporary_files.sh
+++ temporary_files.sh
@@ -1,1 +1,12 @@
+#/bin/bash
+#Using mktemp command
+#Mr. Fana
 
+
+file_name=''
+
+read -p "Write temp file name here:" file_name
+tmp_file=$(mktemp -t $file_name.XXXXXX)
+echo "File $file_name created, the path is:"
+echo ""
+echo $tmp_file
