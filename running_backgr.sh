d3a9c7a0a93d831813b8bff5b9f1948a7edfabcf
greatbrain <erickfania@gmail.com>
2023-01-05 00:08:58 -0400

    Background process script

--- running_backgr.sh
+++ running_backgr.sh
@@ -3,3 +3,11 @@
 #To keep any process or script working in background, we justo add an "&" at
 #The end of the command.
 
+#If we want to let the script/command running even the 
+#Terminal is closed, we must use the 'nohup command' command.
+
+echo "This is an example of background runnin"
+exit
+
+
+
